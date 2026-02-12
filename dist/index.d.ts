f9f2187f7a01f67e4ce7a9bee2e67443cf2693f2
HST-WereDi <hst@sgweredi.nl>
2026-02-12 21:08:49 +0100

    docs en dist uit repo gehaald

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export type Mark = "X" | "O" | ".";
-export type Move = {
-    boardRow: number;
-    boardCol: number;
-    cellRow: number;
-    cellCol: number;
-};
-//# sourceMappingURL=index.d.ts.map
